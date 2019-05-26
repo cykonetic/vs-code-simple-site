37722ab31c29b99adc187f466427148897c8b2bf
Nicholai Bush <nicholaibush@yahoo.com>
2019-05-26 03:00:52 -0400

    getting cmder to work in vs code isn't simple

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict'
 
-console.log('Here\'s a hidden message')
-
 let now = new Date();
 let today = now.toDateString()
 let el = document.getElementById('date')
 el.innerHTML = today
+
+console.log('Here\'s a hidden message')
